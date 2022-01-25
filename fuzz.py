9f77897ccb60992f448b892c00fdf82a006c29bd
Amy Do <ahd6901@rit.edu>
2022-01-25 13:26:28 -0500

    fix attempt

--- fuzz.py
+++ fuzz.py
@@ -22,27 +22,30 @@ def main():
     browser = mechanicalsoup.StatefulBrowser(user_agent='MechanicalSoup')
 
     # Go to setup.php, submit new creat/set
-    browser.open(args.url + 'dvwa/setup.php')
-    browser.post(args.url + 'dvwa/setup.php#')
+    browser.open(args.url + 'setup.php')
+    browser.post(args.url + 'setup.php#')
 
     # Navigate to url, It should redirect to login.php
-    browser.open(args.url + 'dvwa/')
-    time.sleep(3)
+    browser.open(args.url)
+
     # Submit login form
-    form = browser.select_form('form')
+    form = browser.select_form()
+
+
     browser['username'] = 'admin'
     browser['password'] = 'password'
     browser.submit_selected()
 
     # Set security level to easy, and submit the form
-    browser.open(args.url + 'dvwa/security.php')
+    browser.open(args.url + 'security.php')
     form2 = browser.select_form('form[method="POST"]')
     browser['security'] = 'low'
     browser.submit_selected()
 
     # Return to main page. Print out its HTML to console
-    browser.open(args.url + 'dvwa/')
-    print(str(browser.get_current_page()))
+    browser.open(args.url)
+    browser.launch_browser()
+    #print(str(browser.get_current_page()))
 
 
 if __name__ == '__main__':
