1096890b5239fe74fc74c57928756034d55c04a3
Amy Do <ahd6901@rit.edu>
2022-01-25 17:10:28 -0500

    code cleanup

--- fuzz.py
+++ fuzz.py
@@ -31,22 +31,19 @@ def main():
 
     # Submit login form
     form = browser.select_form()
-
     browser['username'] = 'admin'
     browser['password'] = 'password'
     browser.submit_selected()
 
     # Set security level to easy, and submit the form
     browser.open(args.url + 'security.php')
-    print(str(browser.get_current_page()))
     form2 = browser.select_form()
     browser['security'] = 'low'
     browser.submit_selected()
 
     # Return to main page. Print out its HTML to console
     browser.open(args.url)
-    browser.launch_browser()
-    #print(str(browser.get_current_page()))
+    print(str(browser.get_current_page()))
 
 
 if __name__ == '__main__':
