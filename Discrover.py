35ece32bdc32b8864f83ae346a3986f526e74d50
Amy Do <ahd6901@rit.edu>
2022-02-02 18:35:36 -0500

    added

--- Discrover.py
+++ Discrover.py
@@ -4,15 +4,16 @@ def discover(browser, url, word_list, extension_list):
     possible_paths = []
     correct_guesses = []
 
+
+
     # get possible paths
     for word in word_list:
         for extension in extension_list:
             possible_paths.append('/' + word + extension)
-    # check url+possible_path
+    # Validate possible paths
     for path in possible_paths:
         full_url = url + path
         res = browser.open(full_url)
         if res.status_code == 200:
             correct_guesses.append(full_url)
-    print('Correct guesses: '+str(correct_guesses))
     pass
