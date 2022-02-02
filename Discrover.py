0b2dc54660aab089a330168a06933222374bfc75
Amy Do <ahd6901@rit.edu>
2022-02-02 18:22:06 -0500

    Discover guessed pages

---
+++ Discrover.py
@@ -0,0 +1,18 @@
+def discover(browser, url, word_list, extension_list):
+    cookies = browser.get_cookiejar()
+    # page guessing
+    possible_paths = []
+    correct_guesses = []
+
+    # get possible paths
+    for word in word_list:
+        for extension in extension_list:
+            possible_paths.append('/' + word + extension)
+    # check url+possible_path
+    for path in possible_paths:
+        full_url = url + path
+        res = browser.open(full_url)
+        if res.status_code == 200:
+            correct_guesses.append(full_url)
+    print('Correct guesses: '+str(correct_guesses))
+    pass
--- fuzz.py
+++ fuzz.py
@@ -1,7 +1,6 @@
-
 import mechanicalsoup
 import argparse
-import time
+from Discrover import discover
 
 
 def main():
@@ -9,25 +8,45 @@ def main():
     parser.add_argument('action', help="Commands[discover|test]")
     parser.add_argument('url', help="URL")
     parser.add_argument('--custom-auth',
-                        help="Signal that the fuzzer should use hard-coded authentication for a specific application (e.g. dvwa).")
+                        help="Signal that the fuzzer should use hard-coded authentication for a specific application "
+                             "(e.g. dvwa).")
+    parser.add_argument('--common-words',
+                        help='Newline-delimited file of common words to be used in page guessing. Required.')
+    parser.add_argument('--extensions',
+                        help="Newline-delimited file of path extensions, e.g. '.php'. Optional. Defaults to '.php'' "
+                             "and the empty string if not specified")
     args = parser.parse_args()
 
-    # input validation
     if args.action == 'discover' or args.action == 'test':
         pass
     else:
-        exit
+        exit()
 
-    # Connect
+    # Checks, args, gets word_list and extension_list
+    if args.common_words is None:
+        exit()
+    word_list = read_file_content(args.common_words)
+    if args.extensions is None:
+        extension_list = ['.php']
+    else:
+        extension_list = read_file_content(args.extensions)
+
+    #login
+    browser = login(args.url)
+
+    #discover
+    discover(browser, args.url, word_list, extension_list)
+
+
+def login(url):
     browser = mechanicalsoup.StatefulBrowser(user_agent='MechanicalSoup')
-
     # Go to setup.php, submit new creat/set
-    browser.open(args.url + 'setup.php')
+    browser.open(url + 'setup.php')
     browser.select_form()
     browser.submit_selected()
 
     # Navigate to url, It should redirect to login.php
-    browser.open(args.url)
+    browser.open(url)
 
     # Submit login form
     form = browser.select_form()
@@ -36,14 +55,21 @@ def main():
     browser.submit_selected()
 
     # Set security level to easy, and submit the form
-    browser.open(args.url + 'security.php')
+    browser.open(url + 'security.php')
     form2 = browser.select_form()
     browser['security'] = 'low'
     browser.submit_selected()
 
     # Return to main page. Print out its HTML to console
-    browser.open(args.url)
-    print(str(browser.get_current_page()))
+    browser.open(url)
+    print("Logged into DVWA.")
+
+    return browser
+
+
+def read_file_content(filename):
+    with open(filename) as f:
+        return f.read().splitlines()
 
 
 if __name__ == '__main__':
